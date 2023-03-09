a5dd13a622f473ad7fa0b2bf32899156df7be27f
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-03-09 19:29:23 +0900

    무한 루프를 피하기 위해 @JSONIGNORE 단방향으로 설정

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * xToOne(M to O, O to O)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@Controller
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
